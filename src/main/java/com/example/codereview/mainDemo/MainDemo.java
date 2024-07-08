558e9fbb92a2cb99df22e6ac0cef788035e25e9e
许英全 <769552609@qq.com>
2024-07-08 13:43:21 +0800

    叼你  测试成功呀

--- src/main/java/com/example/codereview/mainDemo/MainDemo.java
+++ src/main/java/com/example/codereview/mainDemo/MainDemo.java
@@ -3,5 +3,6 @@ package com.example.codereview.mainDemo;
 public class MainDemo {
     public static void main(String[] args) {
         System.out.println("叼你老母！");
+        System.out.println("叼你老母！");
     }
 }
