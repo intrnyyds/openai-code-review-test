e372611d8c7075216a4cef01e9fc6ac9d0492453
许英全 <769552609@qq.com>
2024-07-08 13:25:01 +0800

    base

---
+++ src/main/java/com/example/openaicodereviewtest/OpenaiCodeReviewTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.openaicodereviewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiCodeReviewTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiCodeReviewTestApplication.class, args);
+	}
+
+}
